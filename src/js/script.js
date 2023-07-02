51c1b72c3f979649827df5eaa989b91519840aa5
SamColt36 <samuelss.contato@gmail.com>
2023-07-02 08:42:15 -0300

    Adicionando lógica para menu rolagem de page

--- src/js/script.js
+++ src/js/script.js
@@ -17,3 +17,14 @@ for (let i = 0; i < btnCadastrar.length; i++) {
 		}
 	});
 }
+
+const checkboxMobile = document.getElementById('checkbox-mobile')
+const tela = document.getElementById('#screen')
+let contador = 1
+
+checkboxMobile.addEventListener('click', function () {
+	if (contador % 2 == 0 & contador % 2 > 0) {
+		console.log(contador)
+	} 
+	contador++
+});
